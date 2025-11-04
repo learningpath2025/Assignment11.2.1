30ac419e977818a763cae98327d341d52e505d4f
learningpath2025 <learningpath2025@outlook.com>
2025-11-04 10:15:26 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Assignment11._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] prices = [7, 6, 4, 3, 1];
+            Console.WriteLine(MaxProfit(prices));
+            Console.ReadLine();
+        }
+
+        public static int MaxProfit(int[] prices)
+        {
+            int maxP = 0;
+            int minBuy = prices[0];
+
+            foreach (int sell in prices)
+            {
+                maxP = Math.Max(maxP, sell - minBuy);
+                minBuy = Math.Min(minBuy, sell);
+            }
+            return maxP;
+        }
+    }
+}
